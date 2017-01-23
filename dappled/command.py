af6962bfe83faf4b5918c01c65944b505a3afa41
Lawrence Hon <hon@cs.stanford.edu>
2017-01-23 15:27:39 -0800

    handle unicode

--- dappled/command.py
+++ dappled/command.py
@@ -284,16 +284,16 @@ def download_notebook_data(id, include_env=False):
 
 def write_notebook_data(data, path='', write_environment_yml=False):
     with open(os.path.join(path, 'dappled.yml'), 'w') as f:
-        print(data['dappled_yml'], file=f)
+        f.write(data['dappled_yml'].encode('utf8'))
 
     yml = ruamel.yaml.load(data['dappled_yml'], ruamel.yaml.RoundTripLoader) 
     filename = os.path.basename(yml['filename'])
     with open(os.path.join(path, filename), 'w') as f:
-        print(data['notebook'], file=f)
+        f.write(data['notebook'].encode('utf8'))
 
     if write_environment_yml:
         with open(os.path.join(path, 'environment.yml'), 'w') as f:
-            print(data['env'], file=f)
+            f.write(data['env'].encode('utf8'))
 
 def setup_published(id):
     if '.' in id:
